0afad1dc7afae15ed9edb517a277fb9ef8978a27
Nolin Brashears <61281833+nbrashears@users.noreply.github.com>
2025-08-26 22:35:01 -0400

    Update randomWallpaper.sh
    Added functionality so that it doesn't reselect the same wallpaper twice in a row.

--- randomWallpaper.sh
+++ randomWallpaper.sh
@@ -3,8 +3,30 @@
 # Folder with wallpapers
 WALLPAPER_DIR="$HOME/PATH/TO/YOUR/BACKGROUNDS/FOLDER"
 
-# Pick a random image from the folder
-RANDOM_WP=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+# Save that random wallpaper to .last_wallpaper
+LAST_WALLPAPER_FILE="$HOME/.config/i3/.last_wallpaper"
 
-# Set it as the background
-feh --bg-fill "$RANDOM_WP"
+# Get a list of all wallpapers
+wallpapers=("$WALLPAPER_DIR"/*)
+
+# Check if .last_wallpaper exists, if so, read it
+if [[ -f "$LAST_WALLPAPER_FILE" ]]; then
+	last_wallpaper=$(<"$LAST_WALLPAPER_FILE")
+else
+	last_wallpaper=""
+fi
+
+# Build a new list, excluding the most recent background
+filtered=()
+for wp in "${wallpapers[@]}"; do
+	[[ "$wp" != "$last_wallpaper" ]] && filtered+=("$wp")
+done
+
+# Pick a new random wallpaper from the filtered list
+new_wallpaper="${filtered[RANDOM % ${#filtered[@]}]}"
+
+# Set the new wallpaper
+feh --bg-scale "$new_wallpaper"
+
+# Save that in last_wallpaper
+echo "$new_wallpaper" > "$LAST_WALLPAPER_FILE"
